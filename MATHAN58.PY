5e23dcc852e74f9bfbd18b70837ce630e49ec282
Mathanpython77 <muneesmathan497@gmail.com>
2024-11-08 15:14:43 +0530

    add new file

---
+++ MATHAN58.PY
@@ -0,0 +1,10 @@
+a=int(input("enter a no1:"))
+b=int(input("enter a no2:"))
+c=int(input("enter a no3:"))
+if a>b and a>c :
+    print(a,"is greater than ",b, "and",c)
+elif b>a and b>c:
+    print(b,"is greater than",a,"and",c) 
+else:
+    print(c,"is greater than ",a, "and",b)
+           
